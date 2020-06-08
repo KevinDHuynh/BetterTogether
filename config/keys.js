089047939d967eb295b68dc7a4d6f6cd71b441f4
KevinDHuynh <wingler101@gmail.com>
2020-06-08 12:15:03 -0400

    Connection testing

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://admin:E74B9ZXC!zHr@bettertogetherdb-qxpai.azure.mongodb.net/data?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+//Bodyparser
+app.use(bodyParser.json());
+
+//DB
+const db = require('./config/keys').mongoURI;
+
+//connect db
+mongoose.connect(db)
+    .then(() => console.log('MongoDB Connection Successful'))
+    .catch(err => console.log(err));
+
+    const port = process.env.port || 5000;
+
+    app.listen(port, () => console.log('Server started on port ${port}'));
+    
