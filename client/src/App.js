0cdbbdcc27fcdacd2ef79adfa308b9fda42941d9
KevinDHuynh <wingler101@gmail.com>
2020-06-24 10:36:30 -0400

    Insert MaterialUI Component Test

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+import AppBar from './components/AppBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <AppBar></AppBar>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   );
 }
-
+//ReactDOM.render(<App />, document.querySelector('#App'));
 export default App;
