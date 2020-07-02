33e1bbd50e8a5ec01009603d1e6abb20ec3a1210
Jake Fidrocki <jakefidrocki@Jakes-MacBook-Air.local>
2020-07-02 10:04:11 -0400

    added  cards  for the foundation, and ending cta, and fixed the login buttons; accidentally had files on wrong branch, I think I have the up to date files in this push

--- Better_Together_Landing_Page/modal.js
+++ Better_Together_Landing_Page/modal.js
@@ -1,15 +1,42 @@
 // modal for sign up
+<<<<<<< Updated upstream
 var modalSignBtn = document.querySelector('.modal-sign-btn');
 var modalSignBg = document.querySelector('.modal-sign-bg');
 var modalSignClose = document.querySelector('.modal-sign-close');
 
 modalSignBtn.addEventListener('click',function(){
+=======
+// var modalSignBtn = document.querySelector('.modal-sign-btn');
+var modalSignBtn1 = document.querySelector('#signBtn1');
+var modalSignBtn2 = document.querySelector('#ctaBigBtn');
+var modalSignBtn3 = document.querySelector('#signBtn3');
+
+
+var modalSignBg = document.querySelector('.modal-sign-bg');
+var modalSignClose = document.querySelector('.modal-sign-close');
+
+modalSignBtn1.addEventListener('click',function(){
+>>>>>>> Stashed changes
 	modalSignBg.classList.add('bg-active');
 });
 modalSignClose.addEventListener('click',function(){
 	modalSignBg.classList.remove('bg-active');
 });
 
+<<<<<<< Updated upstream
+=======
+modalSignBtn2.addEventListener('click',function(){
+	modalSignBg.classList.add('bg-active');
+	console.log(btn2)	
+});
+modalSignBtn3.addEventListener('click',function(){
+	modalSignBg.classList.add('bg-active');
+	console.log(btn2)	
+});
+// modalSignClose.addEventListener('click',function(){
+// 	modalSignBg.classList.remove('bg-active');
+// });
+>>>>>>> Stashed changes
 //modal for log in
 var modalLogBtn = document.querySelector('.modal-log-btn');
 var modalLogBg = document.querySelector('.modal-log-bg');
