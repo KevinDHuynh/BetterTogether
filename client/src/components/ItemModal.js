65286c33349f53fcef290a2bcc6b275a6149a57c
KevinDHuynh <wingler101@gmail.com>
2020-07-09 17:41:59 -0400

    Add rest of habit add

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Dialog, TextField, Button, DialogTitle,DialogActions, List, ListItem, MenuItem} from '@material-ui/core/';
+import {Dialog, TextField, Button, DialogTitle,DialogActions, List, ListItem, MenuItem, Slider, Typography} from '@material-ui/core/';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
 
@@ -16,20 +16,98 @@ class ItemModal extends Component{
     
     types = [
         {
-          value: 'Physical',
+          value: 'PHYSICAL',
           label: 'Physical',
         },
         {
-          value: 'Sleep',
+          value: 'SLEEP',
           label: 'Sleep',
         },
         {
-          value: 'Creative',
+          value: 'CREATIVE',
           label: 'Creative',
         },
         {
-          value: 'Pleasure',
+          value: 'PLEASURE',
           label: 'Pleasure',
+        },
+      ];
+
+    difficulty = [
+        {
+            value: 'VERY_EASY',
+            label: 'Very Easy'
+        },
+        {
+            value: 'EASY',
+            label: 'Easy'
+        },
+        {
+            value: 'MEDIUM',
+            label: 'Medium'
+        },
+        {
+            value: 'HARD',
+            label: 'Hard'
+        },
+        {
+            value: 'VERY_HARD',
+            label: 'Very Hard'
+        },
+    ];
+
+    time = [
+        {
+          value: 'LESS_THAN_A_WEEK',
+          label: '<1 Week',
+        },
+        {
+            value: 'ONE_WEEK',
+            label: '1 Week',
+        },
+        {
+            value: 'TWO_WEEKS',
+            label: '2 Weeks',
+        },
+        {
+            value: 'THREE_WEEKS',
+            label: '3 Weeks',
+        },
+        {
+            value: 'ONE_MONTH',
+            label: '1 Month',
+        },
+        {
+            value: 'EIGHT_WEEKS',
+            label: '2 Months',
+        },
+        {
+            value: 'TWELVE_WEEKS',
+            label: '3 Months',
+        },
+        {
+            value: 'TWENTYONE_WEEKS',
+            label: '6 Months',
+        },
+        {
+            value: 'ONE_YEAR',
+            label: '1 Year',
+        },
+        {
+            value: 'TWO_YEARS',
+            label: '2 Years',
+        },
+        {
+            value: 'FIVE_YEARS',
+            label: '5 Years',
+        },
+        {
+            value: 'TEN_YEARS',
+            label: '10 Years',
+        },
+        {
+            value: 'GREATER_THAN_TEN_YEARS',
+            label: '>10 Years',
         },
       ];
     
@@ -84,6 +162,46 @@ class ItemModal extends Component{
                             <ListItem>
                                 <TextField select variant="standard" fullWidth="True" label="Type of Habit" name="type" margin="dense" onChange={this.onChange}>
                                 {this.types.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                    </MenuItem>
+                                ))}
+                                </TextField>
+                            </ListItem>
+                            <ListItem>
+                                {/* Predicted Longterm Benefit*/}
+                                <Typography id="discrete-slider" gutterBottom>
+                                    Predicted Longterm Benefit
+                                </Typography>
+                                <Slider
+                                    name="predicted_longterm_benefit"
+                                    defaultValue={5}
+                                    aria-labelledby="discrete-slider"
+                                    valueLabelDisplay="auto"
+                                    step={1}
+                                    marks
+                                    min={1}
+                                    max={10}
+                                />
+                            </ListItem>
+                            <ListItem>
+
+                                {/* Estimated Difficulty*/}
+
+                                <TextField select variant="standard" fullWidth="True" label="Estimated Difficulty" name="estimated_difficulty" margin="dense" onChange={this.onChange}>
+                                {this.difficulty.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                    </MenuItem>
+                                ))}
+                                </TextField>
+
+                            </ListItem>
+                            <ListItem>
+                                {/* Estimated Time */}
+
+                                <TextField select variant="standard" fullWidth="True" label="Estimated Time" name="estimated_time" margin="dense" onChange={this.onChange}>
+                                {this.time.map((option) => (
                                     <MenuItem key={option.value} value={option.value}>
                                     {option.label}
                                     </MenuItem>
