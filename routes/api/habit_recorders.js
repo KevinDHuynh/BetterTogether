b70f60d3bb326fb10a1f93c6bb8f83145b291a21
fossij <fossij@WITPC0EF46C>
2020-07-09 18:39:21 -0400

    Associated habit requests with users

--- routes/api/habit_recorders.js
+++ routes/api/habit_recorders.js
@@ -6,14 +6,23 @@ const router = express.Router();
 const HabitRecorder = require('../../models/HabitRecorder').HabitRecorder;
 
 // GET: /api/habit_recorders --> PUBLIC
-// Access all DB entries
-router.get('/', (req, res) => {
+// Access all DB entries created by current user
+router.get('/', authenticate, (req, res) => {
     HabitRecorder.find()
-        .then(hr => res.json(hr));
+        .then(hr => {
+            // Filter for habits created by current user
+            const habits = []
+            for(i = 0; i < hr.length; i++) {
+                if(hr[i].user_id === req.user.id ) {
+                    habits.push(hr[i])
+                }
+            }
+            res.json(habits);
+        });
 });
 
 // GET: /api/habit_recorders/:id --> PUBLIC
-// Access specified DB entry
+// Access specified DB entry by ID
 router.get('/:id', (req, res) => {
     HabitRecorder.findById(req.params.id)
         .then(hr => res.json(hr));
@@ -31,17 +40,26 @@ router.post('/', authenticate, (req, res) => {
         estimated_time: req.body.estimated_time,
         actual_difficulty: req.body.actual_difficulty,
         actual_time: req.body.actual_time,
-        perceived_benefit: req.body.perceived_benefit
+        perceived_benefit: req.body.perceived_benefit,
+        user_id: req.user.id
     });
 
-    newHabitRecorder.save().then(hr => res.send(hr));
+    newHabitRecorder.save().then(hr => res.json(hr));
 });
 
 // DELETE: /api/habit_recorders/:id --> PRIVATE
-// Delete an existing DB entry
+// Delete an existing DB entry, as long as DB entry owned by current user
 router.delete('/:id', authenticate, (req, res) => {
     HabitRecorder.findById(req.params.id)
-        .then(hr => hr.remove().then(() => res.json({ success: true })))
+        .then(hr => {
+            // Verify user owns habit
+            if(hr.user_id === req.user.id) {
+                hr.remove().then(() => res.json({ success: true }));
+            }
+            else {
+                res.status(404).json({ error_msg: "User unable to delete this habit" });
+            }
+        })
         .catch(err => res.status(404).json({ success: false }));
 });
 
