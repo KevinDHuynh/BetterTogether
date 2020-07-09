1477d83ce73341c8ee3d6a13f265584a7ba8f4f2
KevinDHuynh <wingler101@gmail.com>
2020-07-09 03:38:44 -0400

    fix typo for env

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ mongoose.connect(db, {
     .then(() => console.log('MongoDB Connection Successful'))
     .catch(err => console.log(err));
 
-    const port = process.env.port || 5000;
+    const port = process.env.PORT || 5000;
 
     app.listen(port, () => console.log(`Server started on port ${port}`));
