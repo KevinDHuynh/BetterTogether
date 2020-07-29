20bcaaa01b147e654d324e5c26241fb0c0eb57a4
arcec-cs <arcec.cs@gmail.com>
2020-07-29 12:48:27 -0400

    unpixilized logo; added tagline to Better Together to let user know what better together is; fixed margins on cards; added content to the webiste and made spacing adustments to accomidate them

--- Better_Together_Landing_Page/modal.js
+++ Better_Together_Landing_Page/modal.js
@@ -1,3 +1,4 @@
+// Modals that did not make it into final project because of backend time constraints
 // modal for sign up
 var modalSignBtn1 = document.querySelector('#signBtn1');
 var modalSignBtn2 = document.querySelector('#signBtn2');
@@ -39,3 +40,32 @@ modalLogClose.addEventListener('click',function(){
 	modalLogBg.classList.remove('bg-active');
 });
 
+
+/*
+ HTML for Modals that did not make it into final project because of backend time constraints
+  <!-- modal sign up -->
+  <div class="modal-sign-bg">
+    <div class="modal">
+      <h2>Sign Up For Better Together!</h2>
+      <label for="usernameS">Username:</label>
+      <input type="text" name="usernameS" id="usernameS">
+      <label for="passwordS">Password:</label>
+      <input type="text" name="passwordS" id="passwordS">
+      <button>Sign Up!</button>
+      <span class="modal-sign-close">X</span>  
+    </div>
+  </div>
+
+  <!-- modal log in -->
+  <div class="modal-log-bg">
+    <div class="modal">
+      <h2>Log In To Better Together!</h2>
+      <label for="usernameL">Username:</label>
+      <input type="text" name="usernameL" id="usernameL">
+      <label for="passwordL">Password:</label>
+      <input type="text" name="passwordL" id="passwordL">
+      <button>Sign Up!</button>
+      <span class="modal-log-close">X</span>  
+    </div>
+  </div>
+*/
